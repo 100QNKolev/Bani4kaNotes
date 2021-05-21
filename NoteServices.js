7f17a9c59bb88637074bb055195d02baa4bcaaca
100QNKOLEV <shadoowkrit1@gmail.com>
2021-05-21 15:38:50 +0300

    Added GET  request to EvetServices

--- NoteServices.js
+++ NoteServices.js
@@ -4,6 +4,23 @@ import uuid from 'uuid';
 const BASE_URL = 'https://github.com/100QNKolev/Bani4kaNotes';
 const API_KEY = '60a79799e3b6e02545edab48';
 
+export function getEvents(){
+    return fetch(BASE_URL,
+        {
+            method: 'GET',
+            headers: {
+             'content-type' : 'application/json',
+             'x-apikey' : API_KEY
+            }
+        }
+    )
+    .then(response => response.json())
+    .then(events => events.map(item => ({
+        ...item,
+        date: moment(item.date, "DD/MM/YYYY HH:mm").toDate(),
+    })));
+}
+
 export function addNote({ title, date, description }) {
     return fetch(BASE_URL,
         {
