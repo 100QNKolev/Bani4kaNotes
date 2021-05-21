b14e70e5fcbcd70fee764b4b0eec1800f0918ced
100QNKOLEV <shadoowkrit1@gmail.com>
2021-05-21 15:32:14 +0300

    Added POST request to NoteServices

--- NoteServices.js
+++ NoteServices.js
@@ -4,3 +4,18 @@ import uuid from 'uuid';
 const BASE_URL = 'https://github.com/100QNKolev/Bani4kaNotes';
 const API_KEY = '60a79799e3b6e02545edab48';
 
+export function addNote({ title, date, description }) {
+    return fetch(BASE_URL,
+        {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'x-apikey': API_KEY
+            },
+            body: JSON.stringify({
+                title, date, description, id: uuid()
+            })
+        })
+        .then(result => result.json())
+        .catch(error => console.error(error))
+}
